b8cb7e1f93b7f2b9b7f1ff782e51292f387d6749
Ardulous <rohan.me2014@gmail.com>
2017-12-08 19:33:20 +0530

    UI changed

--- app/src/main/java/co/ardulous/skillindia/MainActivity.java
+++ app/src/main/java/co/ardulous/skillindia/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     //This actually serves a purpose of MainActivity
@@ -12,12 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_splashscreen);
 
-        new Handler().postDelayed(new Runnable() {
+        Thread thread = new Thread() {
+            @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, PanelActivity.class));
-                finish();
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException ex) {
+                    Log.e("Splash Screen", "Interrupt");
+                } finally {
+                    startActivity(new Intent(MainActivity.this, PanelActivity.class));
+                    finish();
+                }
             }
-        }, 3 * 1000);
+        };
+        thread.start();
 
     }
 }
